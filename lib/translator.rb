66b1911793079fa4bd448e7d2a64579c88ae937c
passionate-optimization-7504 <nicholasjardine9@gmail.com>
2019-09-30 18:35:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,18 +3,18 @@
 def load_library(file)
   require "yaml"
   file_hash = YAML.load_file(file)
-  translate_hash = {
-    :get_meaning => {},
-    :get_emoticon => {}
-  }
-  file_hash.each do |meaning, arr|
-    if translate_hash[:get_meaning] = {
-      p meaning
-      p arr[0]
+  #translate_hash = {
+   # :get_meaning => {},
+  #  :get_emoticon => {}
+ # }
+#  file_hash.each do |meaning, arr|
+    #if translate_hash[:get_meaning] = {
+    #  p meaning
+   #   p arr[0]
     #}
-  end
-  #p file_hash
-  p translate_hash
+  #end
+  p file_hash
+  #p translate_hash
 end
 
 def get_japanese_emoticon(file, emoticon)
